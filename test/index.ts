077cd59bec75e2bda174cd6652b6064655125344
Markis Taylor <m@rkis.cc>
2017-04-01 23:18:21 -0700

    raise coverage to 100%

--- test/index.ts
+++ test/index.ts
@@ -4,21 +4,17 @@ import StripWhitespacePlugin from '../src/index';
 
 // initialize objects and create test code with expectations
 const plugin = new StripWhitespacePlugin();
-const code = `const longString = "      x          ";`;
-const expected = `const longString = " x ";`;
 
 // wire up the fake compiler and fake settings
+type TestFunction = (value?: any, value2?: any) => void;
 const fakeAssets = {
-  'test.js': new RawSource(code)
+  'test.js': new RawSource('')
 };
-const fakeCompiler = {
-  options: {
-    devtool: true
-  },
-  plugin: (name: 'compilation', callback) => {
+const fakeCompiler: any = {
+  plugin: (name: 'compilation', callback: TestFunction) => {
     callback({
       assets: fakeAssets,
-      plugin: (name2: 'optimize-chunk-assets', callback2) => {
+      plugin: (name2: 'optimize-chunk-assets', callback2: TestFunction) => {
         callback2([{
           files: ['test.js']
         }], () => {
@@ -29,11 +25,56 @@ const fakeCompiler = {
   }
 };
 
-// execute the plugin
-plugin.apply(fakeCompiler);
+{
+  const code = `const longString = "      x          ";`;
+  const expected = `const longString = " x ";`;
 
-// gather the results
-const result = fakeAssets['test.js'].source();
+  // wire up the fake compiler and fake settings
+  fakeAssets['test.js'] = new RawSource(code);
+  fakeCompiler.options = { devtool: true };
 
-// assert the result matches the expectations
-ok(result === expected, `Result (${result}) did not equal expected (${expected})`);
+  // execute the plugin
+  plugin.apply(fakeCompiler);
+
+  // gather the results
+  const result = fakeAssets['test.js'].source();
+
+  // assert the result matches the expectations
+  ok(result === expected, `Result (${result}) did not equal expected (${expected})`);
+}
+
+{
+  const code = `const longString = "      x          ";`;
+  const expected = `const longString = " x ";`;
+
+  // wire up the fake compiler and fake settings
+  fakeAssets['test.js'] = new RawSource(code);
+  fakeCompiler.options = { devtool: false };
+
+  // execute the plugin
+  plugin.apply(fakeCompiler);
+
+  // gather the results
+  const result = fakeAssets['test.js'].source();
+
+  // assert the result matches the expectations
+  ok(result === expected, `Result (${result}) did not equal expected (${expected})`);
+}
+
+{
+  const code = `const longString = " x ";`;
+  const expected = `const longString = " x ";`;
+
+  // wire up the fake compiler and fake settings
+  fakeAssets['test.js'] = new RawSource(code);
+  fakeCompiler.options = { devtool: false };
+
+  // execute the plugin
+  plugin.apply(fakeCompiler);
+
+  // gather the results
+  const result = fakeAssets['test.js'].source();
+
+  // assert the result matches the expectations
+  ok(result === expected, `Result (${result}) did not equal expected (${expected})`);
+}
